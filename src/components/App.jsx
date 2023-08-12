91df209edcff82e01efb6e3120fa0fa84e2f8099
EvgenKvashuk <105291436+EvgenKvashuk@users.noreply.github.com>
2023-08-12 20:04:28 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import SearchBar from "./Searchbar/Searchbar";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import Button from "./Button/Button";
 import Loader from "./Loader/Loader";
-import Modal from "./Modal/Modal";
+
 
 
 class App extends Component {
